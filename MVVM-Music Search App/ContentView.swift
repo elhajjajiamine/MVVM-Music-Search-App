b7072f1ca4b76da7a5825ddc111a6192f609d3c7
elhajjajiamine <aminselhajjaji@gmail.com>
2021-04-04 15:07:18 +0100

    Initial Commit

---
+++ MVVM-Music Search App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM-Music Search App
+//
+//  Created by elhajjaji on 4/4/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM-Music Search App/MVVM_Music_Search_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_Music_Search_AppApp.swift
+//  MVVM-Music Search App
+//
+//  Created by elhajjaji on 4/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_Music_Search_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
